f34cdb27c317adc44cb5eb87699b5491ff5ad7d8
David Petrizze <dpetrizze@gmail.com>
2020-05-17 07:10:55 -0700

    Rethink stack allocations, implement less wasteful solution

--- znc/compile.lua
+++ znc/compile.lua
@@ -5,6 +5,7 @@
 
 local ir = require 'ir'
 local lc = require 'lc'
+local pprint = require 'pprint'
 
 ----------------------------------------------------------------------------------------------------
 -- Utilities
@@ -48,7 +49,7 @@ local function deepcopy(t)
 end
 
 ----------------------------------------------------------------------------------------------------
--- Allocations
+-- Misc. allocations
 
 -- Returns a subroutine-unique label
 local function new_label(ctx)
@@ -57,70 +58,121 @@ local function new_label(ctx)
   return 'l'..idx
 end
 
--- Function-local storage is allocated via simple stacking
--- TODO: Re-implement unstacking at end of block scope (why was that removed?)
-local function stack_alloc(ctx, size)
-  local idx = ctx.stack_index
-  ctx.stack_index = ctx.stack_index + size
-  return idx, size
+----------------------------------------------------------------------------------------------------
+-- Local variable / block allocations
+
+function type_stack_size(ctx, var_type)
+  if var_type.reference == false and var_type.hard_type == lc.type_int64 then
+    -- int64, one machine word c:
+    return 1
+  end
+  error('Failed to find stack size for type '..tostring(var_type))
 end
 
--- Returns the current stack allocation index
-local function stack_mark(ctx)
-  return ctx.stack_index
+function new_block(ctx)
+  local block_stack = ctx.block_stack
+  local top_block = block_stack[#block_stack]
+  local b
+
+  if top_block then
+    -- Create a new block, inheriting the top (current) block's stack index and scope
+    b = {
+      stack_index = top_block.stack_index,
+      scope = copy_scope(top_block.scope),
+      locals = { },
+    }
+  else
+    -- Create a top-level block
+    b = {
+      stack_index = 0,
+      scope = { },
+      locals = { },
+    }
+  end
+
+  -- Push this block
+  block_stack[#block_stack+1] = b
 end
 
--- Reverts the top of the stack to the given index
-local function stack_cut(ctx, index)
-  ctx.stack_index = index
+function free_block(ctx, b)
+  -- Ensure that there is a block to free
+  local block_stack = ctx.block_stack
+  assert(#block_stack > 0)
+
+  -- Optionally ensure that we're freeing the expected block
+  if b then assert(block_stack[#block_stack] == b) end
+
+  -- This is where destructors would be called, e.g.:
+  -- for i,l in ipairs(b.locals) do ... end
+
+  -- Remove top block
+  block_stack[#block_stack] = nil
 end
 
--- Attempts to create a nameless local variable in the current scope
+-- Attempts to create a local variable in the current block
 --   ctx       : Compiler context
 --   var_type  : lc.variable_type
 --   -> lc.variable
-local function new_local(ctx, var_type)
-  local size
-  if var_type.reference then
-    -- Reference type
-    error('Can\'t create reference-typed locals... yet')
-  else
-    if var_type.hard_type == lc.type_int64 then
-      -- int64, alloc one machine word
-      size = 1
-    else
-      error('Can\'t create non-int64-typed locals... yet')
-    end
-  end
+function new_local(ctx, var_type)
+  -- Find top block
+  local block_stack = ctx.block_stack
+  assert(#block_stack > 0)
+  local b = block_stack[#block_stack]
 
-  if size then
-    -- Create a local entry in the current subroutine
-    local id = ir.create_local(ctx.subroutine, stack_alloc(ctx, size))
-    -- Return constructed variable
-    return lc.variable(var_type, id)
-  else
-    error('No size for local')
-  end
+  -- Calculate necessary storage space, in machine words
+  local size = type_stack_size(ctx, var_type)
+
+  -- Allocate on stack
+  local offset = b.stack_index
+  b.stack_index = b.stack_index + size
+
+  -- Create an entry for a new local in the current subroutine
+  local id = ir.create_local(ctx.subroutine, offset, size)
+  -- Create official variable object
+  local var = lc.variable(var_type, id)
+
+  -- Return constructed variable
+  return var
 end
 
--- Attempts to create a named local variable in the current scope
---   ctx       : Compiler context
---   name      : string
---   var_type  : lc.variable_type
---   -> lc.variable
-local function new_named_local(ctx, name, var_type)
-  if ctx.local_scope[name] then
+function add_to_scope(ctx, name, var)
+  -- Find top block
+  local block_stack = ctx.block_stack
+  assert(#block_stack > 0)
+  local b = block_stack[#block_stack]
+
+  -- Shadowing is expressly forbidden
+  if b.scope[name] then
     report_error(ctx, '`'..name..'` was already declared in this scope.')
-  else
-    -- Allocate this variable
-    local var = new_local(ctx, var_type)
-    -- Associate this variable with the given name in the current scope
-    ctx.local_scope[name] = var
+  end
+
+  -- Associate this variable with the given name in the current scope
+  b.scope[name] = var
+end
+
+-- Returns the declaration corresponding to the named variable
+--   ctx       : Compiler context
+--   name_path : ast.name_path
+--   -> lc.variable
+local function find_variable(ctx, name_path)
+  -- Find top block
+  local block_stack = ctx.block_stack
+  assert(#block_stack > 0)
+  local b = block_stack[#block_stack]
+
+  if #name_path > 1 then
+    io.write('WARNING: Ignoring absolute path during search for variable '..name_path..'\n')
+  end
+
+  local var = b.scope[name_path[1]]
+  if var then
     return var
+  else
+    report_error(ctx, '`'..name_path..'` was not found in this scope.')
   end
 end
 
--- Creates a new function declaration and adds it to the current scope
+-- Creates a new function declaration and adds it to the current module scope
 --   ctx      : Compiler context
 --   name     : string
 --   ast_func : ast.function_declaration
@@ -133,22 +185,6 @@ local function new_function(ctx, name, ast_func, ir_subr)
     local new_decl = lc.function_(name, ast_func, ir_subr)
     ctx.module_scope[name] = new_decl
     return new_decl
-  end
-end
-
--- Returns the declaration corresponding to the named variable
---   ctx       : Compiler context
---   name_path : ast.name_path
---   -> lc.variable
-local function find_variable(ctx, name_path)
-  if #name_path > 1 then
-    io.write('WARNING: Ignoring full path during search for '..name_path..'\n')
-  end
-  local decl = ctx.local_scope[name_path[1]]
-  if decl then
-    return decl
-  else
-    report_error(ctx, '`'..name_path..'` was not found in this scope.')
   end
 end
 
@@ -181,8 +217,8 @@ local emit
 local put_unop_expression
 local put_binop_expression
 
-local put_expression
-local put_expression_h = { }
+local emit_expression
+local emit_expression_h = { }
 
 local put_statement
 local put_statement_h = { }
@@ -197,15 +233,6 @@ local function accept_only_int64(ctx, var)
   if not ( var.variable_type.hard_type == lc.type_int64 and
            var.variable_type.reference == false ) then
     report_error(ctx, 'Only accepting int64 at this time')
-  end
-end
-
-function find_or_declare_lvalue(ctx, ast_lvalue)
-  if ast_lvalue.type == 'declaration' then
-    return new_named_local(ctx, ast_lvalue.name, compute_type(ctx, ast_lvalue.type_specifier))
-  elseif ast_lvalue.type == 'reference' then
-    -- TODO: Index expressions!
-    return find_variable(ctx, ast_lvalue.name_path)
   end
 end
 
@@ -229,7 +256,7 @@ end
 
 function put_unop_expression(ctx, expr, op)
   -- Evaluate subexpression
-  local expr_var = put_expression(ctx, expr)
+  local expr_var = emit_expression(ctx, expr)
   accept_only_int64(ctx, expr_var);
 
   -- Create new slot
@@ -244,11 +271,11 @@ end
 
 function put_binop_expression(ctx, expr_a, expr_b, op)
   -- Evaluate subexpression
-  local expr_a_var = put_expression(ctx, expr_a)
+  local expr_a_var = emit_expression(ctx, expr_a)
   accept_only_int64(ctx, expr_a_var); 
 
   -- Evaluate subexpression
-  local expr_b_var = put_expression(ctx, expr_b)
+  local expr_b_var = emit_expression(ctx, expr_b)
   accept_only_int64(ctx, expr_b_var); 
 
   -- Create new slot
@@ -270,7 +297,7 @@ function put_logical_expression(ctx, expr_a, expr_b, mode)
   accept_only_int64(ctx, out_var); 
 
   -- Evaluate subexpression A
-  local expr_a_var = put_expression(ctx, expr_a)
+  local expr_a_var = emit_expression(ctx, expr_a)
   accept_only_int64(ctx, expr_a_var); 
 
   local reg_out = out_var.ir_id;
@@ -296,7 +323,7 @@ function put_logical_expression(ctx, expr_a, expr_b, mode)
   emit(ctx, ir.label(LABEL_B))
 
   -- Evaluate subexpression B
-  local expr_b_var = put_expression(ctx, expr_b)
+  local expr_b_var = emit_expression(ctx, expr_b)
   accept_only_int64(ctx, expr_b_var); 
 
   local reg_b = expr_b_var.ir_id;
@@ -313,8 +340,8 @@ end
 
 -- Emits code which evaluates the given expression tree and stores it in a new slot
 -- Returns the new slot
-function put_expression(ctx, expr)
-  local h = put_expression_h[expr.type]
+function emit_expression(ctx, expr)
+  local h = emit_expression_h[expr.type]
   if h then
     return h(ctx, expr)
   else
@@ -322,79 +349,79 @@ function put_expression(ctx, expr)
   end
 end
 
-function put_expression_h.integer(ctx, expr)
+function emit_expression_h.integer(ctx, expr)
   local out_var = new_local(ctx, lc.variable_type(false, lc.type_int64, false))
   -- Always int64, so just mov this value
   emit(ctx, ir.mov(out_var.ir_id, ir.literal(tonumber(expr.value))))
   return out_var
 end
 
-function put_expression_h.negate(ctx, expr)
+function emit_expression_h.negate(ctx, expr)
   return put_unop_expression(ctx, expr.expression, ir.neg)
 end
 
-function put_expression_h.binnot(ctx, expr)
+function emit_expression_h.binnot(ctx, expr)
   return put_unop_expression(ctx, expr.expression, ir.bnot)
 end
 
-function put_expression_h.lognot(ctx, expr)
+function emit_expression_h.lognot(ctx, expr)
   return put_unop_expression(ctx, expr.expression, ir.lnot)
 end
 
-function put_expression_h.add(ctx, expr)
+function emit_expression_h.add(ctx, expr)
   return put_binop_expression(ctx, expr.expression_a, expr.expression_b, ir.add)
 end
 
-function put_expression_h.sub(ctx, expr)
+function emit_expression_h.sub(ctx, expr)
   return put_binop_expression(ctx, expr.expression_a, expr.expression_b, ir.sub)
 end
 
-function put_expression_h.mul(ctx, expr)
+function emit_expression_h.mul(ctx, expr)
   return put_binop_expression(ctx, expr.expression_a, expr.expression_b, ir.mul)
 end
 
-function put_expression_h.div(ctx, expr)
+function emit_expression_h.div(ctx, expr)
   return put_binop_expression(ctx, expr.expression_a, expr.expression_b, ir.div)
 end
 
-function put_expression_h.cmpeq(ctx, expr)
+function emit_expression_h.cmpeq(ctx, expr)
   return put_binop_expression(ctx, expr.expression_a, expr.expression_b, ir.eq)
 end
 
-function put_expression_h.cmpneq(ctx, expr)
+function emit_expression_h.cmpneq(ctx, expr)
   return put_binop_expression(ctx, expr.expression_a, expr.expression_b, ir.neq)
 end
 
-function put_expression_h.cmplt(ctx, expr)
+function emit_expression_h.cmplt(ctx, expr)
   return put_binop_expression(ctx, expr.expression_a, expr.expression_b, ir.lt)
 end
 
-function put_expression_h.cmpgt(ctx, expr)
+function emit_expression_h.cmpgt(ctx, expr)
   return put_binop_expression(ctx, expr.expression_a, expr.expression_b, ir.gt)
 end
 
-function put_expression_h.cmpleq(ctx, expr)
+function emit_expression_h.cmpleq(ctx, expr)
   return put_binop_expression(ctx, expr.expression_a, expr.expression_b, ir.leq)
 end
 
-function put_expression_h.cmpgeq(ctx, expr)
+function emit_expression_h.cmpgeq(ctx, expr)
   return put_binop_expression(ctx, expr.expression_a, expr.expression_b, ir.geq)
 end
 
-function put_expression_h.logand(ctx, expr)
+function emit_expression_h.logand(ctx, expr)
   return put_logical_expression(ctx, expr.expression_a, expr.expression_b, 'and')
 end
 
-function put_expression_h.logor(ctx, expr)
+function emit_expression_h.logor(ctx, expr)
   return put_logical_expression(ctx, expr.expression_a, expr.expression_b, 'or')
 end
 
-function put_expression_h.lvalue(ctx, expr)
+function emit_expression_h.lvalue(ctx, expr)
   -- TODO: Index expressions!
   return find_variable(ctx, expr.lvalue.name_path)
 end
 
-function put_expression_h.call(ctx, expr)
+function emit_expression_h.call(ctx, expr)
   local func = find_function(ctx, expr.name_path)
   if #func.ast_function.returns > 0 then
     local dst_type = compute_type(ctx, func.ast_function.returns[1].type_specifier)
@@ -405,7 +432,7 @@ function put_expression_h.call(ctx, expr)
     -- Compute arguments
     local src_vars = { }
     for i,arg_expr in ipairs(expr.arguments) do
-      src_vars[i] = put_expression(ctx, arg_expr)
+      src_vars[i] = emit_expression(ctx, arg_expr)
     end
 
     -- Put other call stuff
@@ -454,13 +481,17 @@ function put_statement(ctx, ast_stmt)
 end
 
 put_statement_h['if'] = function(ctx, ast_stmt)
+  -- Create block for temporaries
+  -- Outside both statements seems to be a safe option.
+  local b = new_block(ctx)
+
   if ast_stmt.else_statement then
     -- If and else
     local LABEL_ELSE = new_label(ctx)
     local LABEL_END = new_label(ctx)
 
     -- Put conditional expression into new temporary
-    local expr_var = put_expression(ctx, ast_stmt.expression)
+    local expr_var = emit_expression(ctx, ast_stmt.expression)
     accept_only_int64(ctx, expr_var); 
     emit(ctx, ir.jz(LABEL_ELSE, expr_var.ir_id))
 
@@ -477,7 +508,7 @@ put_statement_h['if'] = function(ctx, ast_stmt)
     local LABEL_END = new_label(ctx)
 
     -- Put conditional expression into new temporary
-    local expr_var = put_expression(ctx, ast_stmt.expression)
+    local expr_var = emit_expression(ctx, ast_stmt.expression)
     accept_only_int64(ctx, expr_var); 
     emit(ctx, ir.jz(LABEL_END, expr_var.ir_id))
 
@@ -485,29 +516,53 @@ put_statement_h['if'] = function(ctx, ast_stmt)
     put_statement(ctx, ast_stmt.if_statement)
     emit(ctx, ir.label(LABEL_END))
   end
+
+  -- Clear temporaries
+  free_block(ctx, b)
 end
 
 put_statement_h['return'] = function(ctx, ast_stmt)
+  -- Create block for temporaries
+  local b = new_block(ctx)
+
   -- Assign function results to output argument registers
   for i,expr in ipairs(ast_stmt.expressions) do
-    local expr_var = put_expression(ctx, expr)
+    local expr_var = emit_expression(ctx, expr)
     accept_only_int64(ctx, expr_var); 
     emit(ctx, ir.mov(ir.returnid(i - 1), expr_var.ir_id))
   end
   -- Don't forget to emit actual return statement
   emit(ctx, ir.ret())
+
+  -- Clear temporaries
+  free_block(ctx, b)
 end
 
 function put_statement_h.assignment(ctx, ast_stmt)
+  -- Find or create lvalue variables
   local dst_vars = { }
+  local post_declarations = { }
   for i,ast_lvalue in ipairs(ast_stmt.lvalues) do
-    dst_vars[i] = find_or_declare_lvalue(ctx, ast_lvalue)
+    if ast_lvalue.type == 'declaration' then
+      -- Create a local, but don't add it to the current scope just yet. We don't want to allow
+      -- references before the variable has been initialized.
+      dst_vars[i] = new_local(ctx, compute_type(ctx, ast_lvalue.type_specifier))
+      -- Formally declare later
+      table.insert(post_declarations, { ast_lvalue.name, dst_vars[i] })
+    elseif ast_lvalue.type == 'reference' then
+      -- TODO: Index expressions! (WHAT?!)
+      dst_vars[i] = find_variable(ctx, ast_lvalue.name_path)
+    end
   end
+
+  -- Create block for temporaries
+  local b = new_block(ctx)
+
   if #ast_stmt.lvalues == #ast_stmt.expressions then
-    -- Evaluate expression list
+    -- Evaluate RHS expression list
     local src_vars = { }
     for i,expr in ipairs(ast_stmt.expressions) do
-      src_vars[i] = put_expression(ctx, expr)
+      src_vars[i] = emit_expression(ctx, expr)
     end
 
     if #dst_vars ~= 1 then
@@ -519,7 +574,7 @@ function put_statement_h.assignment(ctx, ast_stmt)
       emit_multi_copy(ctx, tmp_vars, src_vars)
       emit_multi_move(ctx, dst_vars, tmp_vars)
     else
-      -- Just assign
+      -- No frills, just assign
       emit_multi_copy(ctx, dst_vars, src_vars)
     end
   elseif #ast_stmt.expressions == 0 then
@@ -530,9 +585,9 @@ function put_statement_h.assignment(ctx, ast_stmt)
       -- Evaluate the call expression's argument expressions
       local src_vars = { }
       for i,expr in ipairs(call_expr.arguments) do
-        src_vars[i] = put_expression(ctx, expr)
+        src_vars[i] = emit_expression(ctx, expr)
       end
-      -- This becomes a simple call
+      -- This becomes a simple call, where lvalues are assigned to function results
       put_call(ctx, dst_vars, find_function(ctx, call_expr.name_path), src_vars)
     else
       report_error(ctx, 'Invalid assignment')
@@ -540,28 +595,42 @@ function put_statement_h.assignment(ctx, ast_stmt)
   else
     report_error(ctx, 'Invalid assignment')
   end
+
+  -- Clear temporaries
+  free_block(ctx, b)
+
+  for i,pd in ipairs(post_declarations) do
+    add_to_scope(ctx, pd[1], pd[2])
+  end
 end
 
 function put_statement_h.block(ctx, ast_stmt)
-  -- Save current scope
-  local local_scope_prev = ctx.local_scope
-  ctx.local_scope = copy_scope(ctx.local_scope)
+  -- New block, new you
+  local b = new_block(ctx)
+
   -- Emit substatements
   for i,ast_substmt in ipairs(ast_stmt) do
     put_statement(ctx, ast_substmt)
   end
-  -- Restore scope
-  ctx.local_scope = local_scope_prev
+
+  -- Bye bye block
+  free_block(ctx, b)
 end
 
 function put_statement_h.call(ctx, ast_stmt)
+  -- Create block for temporaries
+  local b = new_block(ctx)
+
   -- Evaluate the call expression's argument expressions
   local src_vars = { }
   for i,expr in ipairs(ast_stmt.arguments) do
-    src_vars[i] = put_expression(ctx, expr)
+    src_vars[i] = emit_expression(ctx, expr)
   end
   -- This becomes a simple call
   put_call(ctx, { }, find_function(ctx, ast_stmt.name_path), src_vars)
+
+  -- Clear temporaries
+  free_block(ctx, b)
 end
 
 ----------------------------------------------------------------------------------------------------
@@ -573,8 +642,13 @@ local function put_function(ctx, ast_func)
   -- Declare function in the current module scope (we will continue to generate it)
   local func_decl = new_function(ctx, ast_func.name, ast_func, subr)
 
+  ctx.block_stack = { }
+  -- Top-level block for this function (no stack allocations - arguments only)
+  new_block(ctx)
+
+  pprint(ctx.block_stack)
+
   -- Allocate argument registers and populate initial local scope
-  local local_scope = { }
   for i,ast_arg_decl in ipairs(ast_func.arguments) do
     local name = ast_arg_decl.name
     -- Compute the type from the AST type specifier
@@ -582,7 +656,7 @@ local function put_function(ctx, ast_func)
     -- Allocate this argument
     local ir_id = ir.create_argument(subr)
     -- Place in local scope
-    local_scope[name] = lc.variable(arg_type, ir_id)
+    add_to_scope(ctx, name, lc.variable(arg_type, ir_id))
   end
 
   -- Allocate return registers
